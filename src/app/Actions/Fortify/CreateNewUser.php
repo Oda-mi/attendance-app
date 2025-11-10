6fa29095a1a3c56db03bd3bf46138edaa41ebb5b
oda mi <oda2025mi@gmail.com>
2025-11-10 22:31:03 +0900

    Regiserバリデーション設定

---
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        $rules = [
+            'name' => 'required|max:20',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+        ];
+
+        $messages =[
+            'name.required' => 'お名前を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'email.unique' => 'このメールアドレスは既に登録されています',
+            'password.required' => 'パスワードを入力してください',
+            'password.min' => 'パスワードは８文字以上で入力してください',
+            'password.confirmed' => 'パスワードと一致しません',
+        ];
+
+        Validator::make($input, $rules, $messages)->validate();
+
+        return User::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
