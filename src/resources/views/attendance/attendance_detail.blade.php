a0006f7a4318cf09bf306e51bdcb7741ef656d30
oda mi <oda2025mi@gmail.com>
2025-11-15 13:20:35 +0900

    DBデータ表示＋休憩新規入力欄追加

--- src/resources/views/attendance/attendance_detail.blade.php
+++ src/resources/views/attendance/attendance_detail.blade.php
@@ -6,6 +6,10 @@
 
 @section('content')
 
+@php
+use Carbon\Carbon;
+@endphp
+
 <div class="attendance-detail">
     <div class="attendance-detail__title">
         <h1>
@@ -22,7 +26,7 @@
                     <th>名前</th>
                     <td colspan="3">
                         <div class="attendance-detail__user">
-                            <div class="user-name">テスト 太郎</div>
+                            <div class="user-name">{{ $user->name }}</div>
                         </div>
                     </td>
                 </tr>
@@ -30,8 +34,8 @@
                     <th>日付</th>
                     <td colspan="2">
                         <div class="attendance-detail__date">
-                            <div class="date-year">2025年</div>
-                            <div>11月01日</div>
+                            <div class="date-year">{{ $attendance->work_date ? Carbon::parse($attendance->work_date)->format('Y年') : '' }}</div>
+                            <div>{{ $attendance->work_date ? Carbon::parse($attendance->work_date)->format('m月d日') : '' }}</div>
                         </div>
                     </td>
                 </tr>
@@ -39,29 +43,31 @@
                     <th>出勤・退勤</th>
                     <td colspan="3">
                         <div class="attendance-detail__time-inputs">
-                            <input type="text" name="start_time" value="09:00">
+                            <input type="text" name="start_time" value="{{ $attendance->start_time ? Carbon::parse($attendance->start_time)->format('H:i') : '' }}">
                             <span>～</span>
-                            <input type="text" name="end_time" value="18:00">
+                            <input type="text" name="end_time" value="{{ $attendance->end_time ? Carbon::parse($attendance->end_time)->format('H:i') : '' }}">
                         </div>
                     </td>
                 </tr>
+                @foreach($breaks as $index => $break)
                 <tr>
-                    <th>休憩</th>
+                    <th>{{ $index === 0 ? '休憩' : '休憩' . ($index + 1) }}</th>
                     <td colspan="3">
                         <div class="attendance-detail__time-inputs">
-                            <input type="text" name="start_time" value="09:00">
+                            <input type="text" name="break_start[{{ $index }}]" value="{{ $break->start_time ? Carbon::parse($break->start_time)->format('H:i') : '' }}">
                             <span>～</span>
-                            <input type="text" name="end_time" value="18:00">
+                            <input type="text" name="break_end[{{ $index }}]" value="{{ $break->end_time ? Carbon::parse($break->end_time)->format('H:i') : '' }}">
                         </div>
                     </td>
                 </tr>
+                @endforeach
                 <tr>
-                    <th>休憩２</th>
+                    <th>休憩{{ $breaks->count() + 1 }}</th>
                     <td colspan="3">
                         <div class="attendance-detail__time-inputs">
-                            <input type="text" name="start_time" value="">
+                            <input type="text" name="break_start[{{ $breaks->count() }}]" value="">
                             <span>～</span>
-                            <input type="text" name="end_time" value="">
+                            <input type="text" name="break_end[{{ $breaks->count() }}]" value="">
                         </div>
                     </td>
                 </tr>
