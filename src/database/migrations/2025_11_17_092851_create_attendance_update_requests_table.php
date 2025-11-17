737fdf2fd2bfe56711eed9bd207783b8aaf2fa66
oda mi <oda2025mi@gmail.com>
2025-11-17 22:55:22 +0900

    attendance_update_requestテーブル作成

---
+++ src/database/migrations/2025_11_17_092851_create_attendance_update_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceUpdateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_update_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('attendance_id')->constrained()->cascadeOnDelete();
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->json('breaks')->nullable();
+            $table->text('note')->nullable();
+            $table->string('status', 20)->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_update_requests');
+    }
+}
