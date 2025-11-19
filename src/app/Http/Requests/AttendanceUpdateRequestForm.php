627ce381f5a99758e08920b2f9e80541e48e26b3
oda mi <oda2025mi@gmail.com>
2025-11-19 15:43:28 +0900

    カスタムバリデーションwithValidator()メソッド内の変数名修正

--- src/app/Http/Requests/AttendanceUpdateRequestForm.php
+++ src/app/Http/Requests/AttendanceUpdateRequestForm.php
@@ -44,27 +44,27 @@ class AttendanceUpdateRequestForm extends FormRequest
                 $validator->errors()->add('time', '出勤時間または退勤時間を入力してください');
             }
 
-        // 出勤・退勤のタイムスタンプ
-            $startTs = $startTime ? strtotime($startTime) : null;
-            $endTs = $endTime ? strtotime($endTime) : null;
+            // 出勤・退勤のタイムスタンプ
+            $workStartTimestamp = $startTime ? strtotime($startTime) : null;
+            $workEndTimestamp   = $endTime ? strtotime($endTime) : null;
 
-        // 出勤・退勤チェック
-            if ($startTs && $endTs && $startTs > $endTs) {
+            // 出勤時間・退勤時間チェック
+            if ($workStartTimestamp && $workEndTimestamp && $workStartTimestamp > $workEndTimestamp) {
                 $validator->errors()->add('start_time', '出勤時間もしくは退勤時間が不適切な値です');
             }
 
-        // 休憩チェック
+            // 休憩チェックのタイムスタンプ
             foreach ($breakStartTimes as $index => $breakStart) {
-                $breakStartTs = $breakStart ? strtotime($breakStart) : null;
-                $breakEndTs = isset($breakEndTimes[$index]) ? strtotime($breakEndTimes[$index]) : null;
+                $breakStartTimestamp = $breakStart ? strtotime($breakStart) : null;
+                $breakEndTimestamp   = isset($breakEndTimes[$index]) ? strtotime($breakEndTimes[$index]) : null;
 
             // 休憩開始時間が出勤時間より前、もしくは退勤時間より後
-                if ($breakStartTs && ($breakStartTs < $startTs || $breakStartTs > $endTs)) {
+                if ($breakStartTimestamp && ($breakStartTimestamp < $workStartTimestamp || $breakStartTimestamp > $workEndTimestamp)) {
                     $validator->errors()->add("break_start.$index", '休憩時間が不適切な値です');
                 }
 
             // 休憩終了時間が退勤時間より後、もしくは開始時間より前
-                if ($breakEndTs && ($breakEndTs > $endTs || ($breakStartTs && $breakEndTs < $breakStartTs))) {
+                if ($breakEndTimestamp && ($breakEndTimestamp > $workEndTimestamp || ($breakStartTimestamp && $breakEndTimestamp < $breakStartTimestamp))) {
                     $validator->errors()->add("break_end.$index", '休憩時間もしくは退勤時間が不適切な値です');
                 }
             }
@@ -78,4 +78,5 @@ class AttendanceUpdateRequestForm extends FormRequest
             'note.required' =>'備考を入力してください',
         ];
     }
+
 }
