460dad7a5293803a8170323b73160180e790a346
oda mi <oda2025mi@gmail.com>
2025-11-19 22:53:20 +0900

    申請一覧ボタンのリンクルートパス追加

--- src/resources/views/attendance/states/attendance_after_work.blade.php
+++ src/resources/views/attendance/states/attendance_after_work.blade.php
@@ -8,7 +8,7 @@
 
     <ul class="header__buttons">
         <li><a href="{{ route('attendance.list') }}" class="button button--afterwork">今月の出勤一覧</a></li>
-        <li><a href="" class="button button--afterwork">申請一覧</a></li>
+        <li><a href="{{ route('stamp_correction_request.list') }}" class="button button--afterwork">申請一覧</a></li>
         <li>
             <form action="{{ route('logout') }}" method="post">
             @csrf
