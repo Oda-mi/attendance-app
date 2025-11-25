62eaf022a6d5ca6279cf510beec60624e40cab81
oda mi <oda2025mi@gmail.com>
2025-11-25 15:07:21 +0900

    index()メソッド修正＋approve()メソッド作成

--- src/app/Http/Controllers/StampCorrectionRequestController.php
+++ src/app/Http/Controllers/StampCorrectionRequestController.php
@@ -11,17 +11,54 @@ class StampCorrectionRequestController extends Controller
     {
         $user = auth()->user();
 
-        $pendingRequests = AttendanceUpdateRequest::where('user_id', $user->id)
-                                ->where('status', 'pending')
-                                ->orderBy('work_date', 'asc')
-                                ->get();
+        $layout = $user->is_admin
+            ? 'layouts.admin'
+            : 'layouts.auth';
 
-        $approvedRequests = AttendanceUpdateRequest::where('user_id', $user->id)
-                                ->where('status', 'approved')
-                                ->orderBy('work_date', 'asc')
-                                ->get();
+        if($user->is_admin){
 
-        return view('stamp_correction_request.request_list',compact('pendingRequests', 'approvedRequests'));
+            $pendingRequests = AttendanceUpdateRequest::with('user')
+                                                        ->where('status', 'pending')
+                                                        ->orderBy('work_date', 'asc')
+                                                        ->get();
+
+            $approvedRequests = AttendanceUpdateRequest::with('user')
+                                                        ->where('status', 'approved')
+                                                        ->orderBy('work_date', 'asc')
+                                                        ->get();
+
+        } else {
+
+            $pendingRequests = AttendanceUpdateRequest::where('user_id', $user->id)
+                                                        ->where('status', 'pending')
+                                                        ->orderBy('work_date', 'asc')
+                                                        ->get();
+
+            $approvedRequests = AttendanceUpdateRequest::where('user_id', $user->id)
+                                                        ->where('status', 'approved')
+                                                        ->orderBy('work_date', 'asc')
+                                                        ->get();
+        }
+
+        return view('stamp_correction_request.request_list',compact(
+            'layout',
+            'pendingRequests',
+            'approvedRequests',
+            ));
     }
+
+    public function approve($id)
+    {
+        $requestData = AttendanceUpdateRequest::with(['user', 'attendance.breaks'])
+                                                ->findOrFail($id);
+
+        $breaks = $requestData->attendance ? $requestData->attendance->breaks : collect();
+
+        return view('stamp_correction_request.request_approve', compact(
+            'requestData',
+            'breaks'
+        ));
+    }
+
 
 }
