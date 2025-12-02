4f54cdf86a3285d02e26ae6f0d428b21e14373d5
oda mi <oda2025mi@gmail.com>
2025-12-03 08:29:00 +0900

    管理側：休憩時間表示条件分岐修正

--- src/resources/views/admin/attendance/daily_list.blade.php
+++ src/resources/views/admin/attendance/daily_list.blade.php
@@ -68,7 +68,7 @@ use Carbon\Carbon;
                     <td>{{ $attendance->start_time ? Carbon::parse($attendance->start_time)->format('H:i') : '' }}</td>
                     <td>{{ $attendance->end_time ? Carbon::parse($attendance->end_time)->format('H:i') : '' }}</td>
                     <td>
-                        @if($attendance->start_time && $attendance->end_time)
+                        @if($attendance->breaks && $attendance->breaks->count())
                             {{ gmdate('H:i', $attendance->breakTotal ?? 0) }}
                         @else
                             {{-- 出勤・退勤の両方がない場合、または退勤していない場合は空白 --}}
