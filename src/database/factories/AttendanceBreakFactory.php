8b5c3db644eaabef6725e191f840ea61c59d6c0f
oda mi <oda2025mi@gmail.com>
2025-12-03 22:58:22 +0900

    休憩AttendanceBreakFactory作成

---
+++ src/database/factories/AttendanceBreakFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\AttendanceBreak;
+use App\Models\Attendance;
+use Carbon\Carbon;
+
+class AttendanceBreakFactory extends Factory
+{
+    protected $model = AttendanceBreak::class;
+
+    public function definition()
+    {
+        $start = Carbon::now()->setTime(12, 0);
+        $end   = Carbon::now()->setTime(13, 0);
+
+        return [
+            'attendance_id' => Attendance::factory(),
+            'start_time'    => $start,
+            'end_time'      => $end,
+        ];
+    }
+}
